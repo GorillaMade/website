f6eb315d5206389698d5dee167f4c5a77a2b12df
Timothy Remst <11875728+tjaynl@users.noreply.github.com>
2026-08-25 23:05:07 +0200

    My commit
    This is my first commit

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+import { defineConfig, fontProviders } from "astro/config";
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://gorillamade.nl",
+  integrations: [alpinejs()],
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Sora",
+      cssVariable: "--ff-display",
+      weights: [600, 700, 800],
+      styles: ["normal"],
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Plus Jakarta Sans",
+      cssVariable: "--ff-body",
+      weights: [400, 500, 600, 700, 800],
+      styles: ["normal"],
+    },
+  ],
+});
